92581b1420ac21f3c01f9f587c8a928d4bc2c1c6
bozhi Tsai <bozhi9004@gmaill.com>
2021-03-10 04:06:20 -0800

    revise

--- hw1.py
+++ hw1.py
@@ -26,23 +26,18 @@ with open(cwb_filename) as csvfile:
 
 # Retrive ten data points from the beginning.
 
-## 訂定目標的字典，儲存[最大值, 最小值]，預設為[-99, -99]
-traget = {"C0A880":[-99,-99], "C0F9A0":[-99,-99], "C0G640":[-99,-99], "C0R190":[-99,-99], "C0X260":[-99,-99]}
+## 訂定目標的字典，儲存[累積值, 數量]
+target = {"C0A880":[0, 0], "C0F9A0":[0, 0], "C0G640":[0, 0], "C0R190":[0, 0], "C0X260":[0, 0]}
 for row in data:                                      # 跑過所有資料
-   if row['station_id'] in traget:                    # 若此資料是目標
-      WSDS = float(row['WDSD'])                       # 儲存WSDS值
-      if WSDS != -999.000 and WSDS != -99.000:        # 若WSDS值為有效資料
-         if traget[row['station_id']] == [-99,-99]:   # 儲存第一筆數據
-            traget[row['station_id']] = [WSDS, WSDS]
-         if WSDS > traget[row['station_id']][0]:      # WSDS值若大於目標之最大值，儲存至最大值
-            traget[row['station_id']][0] = WSDS
-         if WSDS < traget[row['station_id']][1]:      # WSDS值若小於目標之最小值，儲存至最小值
-            traget[row['station_id']][1] = WSDS
+   if row['station_id'] in target:                    # 若此資料是目標
+      PRES = float(row['PRES'])                       # 儲存PRES值
+      if PRES != -999.000 and PRES != -99.000:        # 若PRES值為有效資料
+         target[row['station_id']] = [target[row['station_id']][0] + PRES, target[row['station_id']][1] + 1]   # 儲存數據
 ## 整理輸出
 target_data = []
-for dat in traget:                                    # 跑過目標字典中所有目標 
-   if traget[dat] != [-99,-99]:                       # 若有數據更新最大最小值
-      target_data += [[dat, traget[dat][0] - traget[dat][1]]]  # 新增至輸出，輸出名字與最大最小值的差
+for dat in target:                                    # 跑過目標字典中所有目標 
+   if target[dat][1] != 0:                            # 若有有效數據
+      target_data += [[dat, target[dat][0] / target[dat][1]]]  # 新增至輸出，輸出平均
    else:
       target_data += [[dat, None]]                    # 若無數據，輸出名字與None
 #=======================================
